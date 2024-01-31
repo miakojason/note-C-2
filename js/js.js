399efee1de3a5d8bf1e948078ca339fd20b6c62c
miakojason <miakojason@gmail.com>
2024-02-01 00:18:24 +0800

    1

--- js/js.js
+++ js/js.js
@@ -19,3 +19,31 @@ function good(id,type,user)
 		}
 	})
 }
+function clean() {
+	$("input[type='text'],input[type=''password],input[type='number'],input[type='radio']").val("");
+}
+function reg(){
+	let user={
+		acc:$("#acc").val(),
+		pw:$("#pw").val(),
+		pw2:$("#pw2").val(),
+		email:$("#email").val()
+	}
+	if(user.acc !='' && user.pw !='' && user.pw2 !='' && user.email !=''){
+		if(user.pw==user.pw2){
+			$.post("./api/chacc.php",{acc:user.acc},(res)=>{
+				if(parseInt(res)==1){
+					alert("帳號重覆")
+				}else{
+					$.post("./api/reg.php",user,(reg)=>{
+						alert('註冊完成')
+					})
+				}
+			})
+		}else{
+			alert("密碼錯誤")
+		}
+	}else{
+		alert("不可空白")
+	}
+}
