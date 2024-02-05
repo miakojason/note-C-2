d924311e1602a7ea9cb6ff2897c7a32250d54035
miakojason <miakojason@gmail.com>
2024-02-05 22:36:00 +0800

    完成16，db練習沒push...

--- index copy.php
+++ index copy.php
@@ -10,10 +10,9 @@
 	<link href="./css/css.css" rel="stylesheet" type="text/css">
 	<script src="./js/jquery-1.9.1.min.js"></script>
 	<script src="./js/js.js"></script>
-</head>
-<style>
-	.pop {
-		background: rgba(51, 51, 51, 0.8);
+	<style>
+		.pop {
+			background: rgba(51, 51, 51, 0.8);
 			color: #FFF;
 			height: 300px;
 			width: 300px;
@@ -22,20 +21,21 @@
 			z-index: 9999;
 			overflow: auto;
 			padding: 10px;
-	}
-</style>
+		}
+	</style>
+</head>
 
 <body>
 
 	<div id="all">
 		<div id="title">
-			<?= date("m 月 d 號 l"); ?> |
-			今日瀏覽: <?= $Total->find(['date' => date('Y-m-d')])['total']; ?> |
+			<?= date("m月d日 l"); ?> |
+			今日瀏覽: <?= $Total->find(['date' => date("Y-m-d")])['total']; ?> |
 			累積瀏覽: <?= $Total->sum('total'); ?>
-			<a href="./index.php" style="float: right;">回首頁</a>
+			<a href="index.php" style='float:right'>回首頁</a>
 		</div>
-		<div id="title2">
-			<a href="./index.php"><img src="./icon/02B01.jpg" alt="健康處進網-回首頁"></a>
+		<div id="title2" title='健康促進網-回首頁'>
+			<a href='index.php'><img src="./icon/02B01.jpg" alt=""></a>
 		</div>
 		<div id="mm">
 			<div class="hal" id="lef">
@@ -46,10 +46,10 @@
 				<a class="blo" href="?do=que">問卷調查</a>
 			</div>
 			<div class="hal" id="main">
-				<div style="display: flex;">
-					<marquee style="width: 80%;">請民眾踴躍投稿電子報，讓電子報成為大家相
-						互交流、分享的園地！詳見最新文章</marquee>
-					<span style="width:16%; display:inline-block;">
+				<div>
+					<marquee style="width:80%; display:inline-block;">請民眾踴躍投稿電子報，讓電子報成為大家相互交流、分享的園地！詳見最新文章</marquee>
+
+					<span style="width:20%; display:inline-block;">
 						<?php
 						if (!isset($_SESSION['user'])) {
 						?>
@@ -58,30 +58,28 @@
 						} else {
 						?>
 							歡迎,<?= $_SESSION['user']; ?>
-							<button onclick="location.hred='./api/logout.php'">登出</button>
+							<button onclick="location.href='./api/logout.php'">登出</button>
 							<?php
 							if ($_SESSION['user'] == 'admin') {
-
-
 							?>
-								<button onclick="location.href='./back.php'">管理</button>
+								<button onclick="location.href='back.php'">管理</button>
 						<?php
 							}
 						}
 						?>
 					</span>
-				</div>
-				<div class="">
-					<?php
-					$do = $_GET['do'] ?? 'main';
-					$file = "./front/{$do}.php";
-					if (file_exists($file)) {
-						include $file;
-					} else {
-						include "./front/main.php";
-					}
-					?>
+					<div class="">
+						<?php
+						$do = $_GET['do'] ?? 'main';
+						$file = "./front/{$do}.php";
+						if (file_exists($file)) {
+							include $file;
+						} else {
+							include "./front/main.php";
+						}
 
+						?>
+					</div>
 				</div>
 			</div>
 		</div>
